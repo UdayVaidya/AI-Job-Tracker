0b8c18f98e6d2eff0f0d1cab0058517806ba5efa
udayvaidya13 <udayvaidya13@gmail.com>
2026-01-02 13:36:56 +0530

    configured MongoDB Integration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+
 
 dotenv.config();
 
@@ -9,11 +12,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+connectDB();
+
 app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "OK", message: "Server is healthy" });
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
