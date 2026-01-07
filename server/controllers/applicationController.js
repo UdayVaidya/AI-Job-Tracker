fa912fa14a1d2f3a42cfd1722eab1eea4e71d861
udayvaidya13 <udayvaidya13@gmail.com>
2026-01-07 18:47:02 +0530

    Day - 6 Created and tested Update, Delete Routes

--- server/controllers/applicationController.js
+++ server/controllers/applicationController.js
@@ -1,53 +1,107 @@
-import Application from '../model/Application.js';
+import Application from "../model/Application.js";
 
-export const createApplication = async(req,res) => {
+export const createApplication = async (req, res) => {
+  try {
+    const { company, role, jobType, status, jobLink, notes } = req.body;
 
-    try {
-        const {
-        company,
-        role,
-        jobType,
-        status,
-        jobLink,
-        notes
-    } = req.body;
-
-    if(!company || !role){
-        return res.status(400).json({
-            message: 'Company and Role are required'
-        });
+    if (!company || !role) {
+      return res.status(400).json({
+        message: "Company and Role are required",
+      });
     }
 
     const application = await Application.create({
-        user: req.user._id,
-        company,
-        role,
-        jobType,
-        status,
-        jobLink,
-        notes
-    })
+      user: req.user._id,
+      company,
+      role,
+      jobType,
+      status,
+      jobLink,
+      notes,
+    });
 
     return res.status(200).json(application);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            message: "Server error"
-        });
-    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
 };
 
-export const getApplication = async(req,res) => {
-    try {
-        const applications = await Application.find({
-            user: req.user._id
-       }).sort({ createdAt: -1});
+export const getApplication = async (req, res) => {
+  try {
+    const applications = await Application.find({
+      user: req.user._id,
+    }).sort({ createdAt: -1 });
 
-       return res.status(200).json(applications);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            message: "Server error"
-        });
+    return res.status(200).json(applications);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+export const updateApplication = async (req, res) => {
+  try {
+    const application = await Application.findOne({ _id: req.params.id });
+
+    if (!application) {
+      return res.status(404).json({
+        message: "Application not Found",
+      });
     }
+
+    if (application.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({
+        message: "Not Authorized",
+      });
+    }
+
+    const updatedApplication = await Application.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json({
+      message: "Application is updated",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+export const deleteApplication = async (req, res) => {
+  try {
+    const application = await Application.findOne({ _id: req.params.id});
+
+    if (!application) {
+      return res.status(404).json({
+        message: "Application not Found",
+      });
+    }
+
+    if (application.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({
+        message: "Not Authorized",
+      });
+    }
+
+    await Application.deleteOne();
+
+    return res.status(200).json({
+      message: "Application is deleted",
+    });
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      message: "Server Error",
+    });
+  }
 };
--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import { protect } from '../middlewares/authMiddleware.js';
-import { createApplication,getApplication } from '../controllers/applicationController.js';
+import { createApplication,
+        getApplication,
+        updateApplication,
+        deleteApplication 
+} from '../controllers/applicationController.js';
 
 const router = express.Router();
 
 router.post("/",protect,createApplication);
 router.get("/",protect,getApplication);
+router.put("/:id",protect,updateApplication);
+router.delete("/:id",protect,deleteApplication);
 
 export default router;
